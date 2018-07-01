6634323c9377e76fb43f8e3b1531c5a686ce7783
Janusz Lenar <malleor@gmail.com>
2018-07-01 22:22:23 +0200

    Made an initial layout

--- usma.py
+++ usma.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello! Usma here."
+def usma():
+    return render_template('usma.html', name='usma')
+
+with app.test_request_context():
+    url_for("static", filename="usma.css")
